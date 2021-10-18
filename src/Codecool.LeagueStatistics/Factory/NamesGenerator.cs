1639c493876b530bd937f66ad6a31bd0219de332
gabriellaszigeti <gbszgt@gmail.com>
2021-10-18 14:16:41 +0100

    feat : add file paths and getrandomvalue method in namagenerator class

--- src/Codecool.LeagueStatistics/Factory/NamesGenerator.cs
+++ src/Codecool.LeagueStatistics/Factory/NamesGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 
 namespace Codecool.LeagueStatistics.Facotry
@@ -7,17 +8,27 @@ namespace Codecool.LeagueStatistics.Facotry
     /// </summary>
     public static class NamesGenerator
     {
+        public static string pathPlayersNames = @"C:\Users\gbszg\source\repos\Codecool\advanced\transition-week\league-statistics-csharp-gabriellaszigeti\data\PlayerNames.txt";
+        public static string pathCitiesNames = @"C:\Users\gbszg\source\repos\Codecool\advanced\transition-week\league-statistics-csharp-gabriellaszigeti\data\CityNames.txt";
+        public static string pathTeamNames = @"C:\Users\gbszg\source\repos\Codecool\advanced\transition-week\league-statistics-csharp-gabriellaszigeti\data\TeamNames.txt";
+
         public static string GetPlayerName()
         {
-            return File.ReadAllLines("PlayerNames.txt").GetRandomValue();
+            return File.ReadAllLines(pathPlayersNames).GetRandomValue();
         }
 
         public static string GetTeamName()
         {
-            var cities = File.ReadAllLines("CityNames.txt");
-            var names = File.ReadAllLines("TeamNames.txt");
+            var cities = File.ReadAllLines(pathCitiesNames);
+            var names = File.ReadAllLines(pathTeamNames);
 
             return cities.GetRandomValue() + " " + names.GetRandomValue();
+        }
+
+        public static int GetRandomValue()
+        {
+            var random = new Random();
+            return random.Next(1, 75);
         }
     }
 }
