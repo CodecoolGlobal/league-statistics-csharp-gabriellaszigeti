becab4a75697e9b341db023edde88132dc252deb
gabriellaszigeti <gbszgt@gmail.com>
2021-10-18 13:43:40 +0100

    feat : implement display methods in display class

--- src/Codecool.LeagueStatistics/View/Display.cs
+++ src/Codecool.LeagueStatistics/View/Display.cs
@@ -10,6 +10,20 @@ namespace Codecool.LeagueStatistics.View
     /// </summary>
     public static class Display
     {
+        public static void PrintAllTeamsSorted(IEnumerable<Team> teams)
+        {
+            IEnumerable<Team> sortedTeams = Model.LeagueStatistics.GetAllTeamsSorted(teams);
+            foreach(var team in sortedTeams)
+            {
+                Console.WriteLine($"Teams name: {team.Name} , and their scores: {team.CurrentPoints()}, draws: {team.Draws}, looses: {team.Losts}, wins: {team.Wins}.");
+            }
+        }
 
+        public static void PrintTeamWithLongestName(IEnumerable<Team> teams)
+        {
+            Team team = Model.LeagueStatistics.GetTeamWithTheLongestName(teams);
+                Console.WriteLine($"Team with the longest name : {team.Name}");
+            }
+        }
     }
-}
+
